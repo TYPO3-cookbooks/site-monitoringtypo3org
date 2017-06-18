dc910d6495e4746b05450482f0e1b98f0957ec88
Steffen Gebert <steffen.gebert@typo3.org>
2017-06-18 17:03:42 +0200

    [TASK] Switch to v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures site-monitoringtypo3org"
-version          "1.0.4"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends          "t3-base",   "~> 0.2.66"
 
